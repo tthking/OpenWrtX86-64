e9bb40532e23aca8f4519a6c90f4eac2232fd26e
tthking <52197005+tthking@users.noreply.github.com>
2024-02-09 12:28:33 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,3 +17,4 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
+echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
